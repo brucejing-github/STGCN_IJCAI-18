913efaac6b84d9786e146d5ca3fa30135c942270
Baoyu Jing <jingby1994@gmail.com>
2020-02-09 01:05:15 -0600

    add all

--- main.py
+++ main.py
@@ -46,9 +46,28 @@ Ks, Kt = args.ks, args.kt
 # blocks: settings of channel size in st_conv_blocks / bottleneck design
 blocks = [[1, 32, 64], [64, 32, 128]]
 
+# # Load wighted adjacency matrix W
+# if args.graph == 'default':
+#     W = weight_matrix(pjoin('./dataset', f'PeMSD7_W_{n}.csv'))
+# else:
+#     # load customized graph weight matrix
+#     W = weight_matrix(pjoin('./dataset', args.graph))
+#
+# # Calculate graph kernel
+# L = scaled_laplacian(W)
+# # Alternative approximation method: 1st approx - first_approx(W, n).
+# Lk = cheb_poly_approx(L, Ks, n)
+# tf.add_to_collection(name='graph_kernel', value=tf.cast(tf.constant(Lk), tf.float32))
+#
+# # Data Preprocessing
+# data_file = f'PeMSD7_V_{n}.csv'
+# n_train, n_val, n_test = 34, 5, 5
+# PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
+# print(f'>> Loading dataset with Mean: {PeMS.mean:.2f}, STD: {PeMS.std:.2f}')
+
 # Load wighted adjacency matrix W
 if args.graph == 'default':
-    W = weight_matrix(pjoin('./dataset', f'PeMSD7_W_{n}.csv'))
+    W = weight_matrix(pjoin('./dataset', f'W_228.csv'))
 else:
     # load customized graph weight matrix
     W = weight_matrix(pjoin('./dataset', args.graph))
@@ -60,7 +79,7 @@ Lk = cheb_poly_approx(L, Ks, n)
 tf.add_to_collection(name='graph_kernel', value=tf.cast(tf.constant(Lk), tf.float32))
 
 # Data Preprocessing
-data_file = f'PeMSD7_V_{n}.csv'
+data_file = f'V_228.csv'
 n_train, n_val, n_test = 34, 5, 5
 PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
 print(f'>> Loading dataset with Mean: {PeMS.mean:.2f}, STD: {PeMS.std:.2f}')
